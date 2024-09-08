8892fd4e56d36d2f51d24f406375d05448f15dcb
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-09-08 20:16:04 +0300

    Add translate button

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -41,6 +41,26 @@ const Navbar = () => {
     // add custom data-theme attribute to html tag required to update theme using DaisyUI
     document.querySelector("html").setAttribute("data-theme", localTheme);
   }, [theme]);
+  // Set the initial language from local storage or default to Arabic
+  const [language, setLanguage] = useState(
+    localStorage.getItem("language") ? localStorage.getItem("language") : "ar"
+  );
+
+  // Toggle language state between Arabic and English
+  // Toggle language state between Arabic and English based on selection
+  const handleLanguageToggle = (selectedLanguage) => {
+    if (selectedLanguage !== language) {
+      setLanguage(selectedLanguage);
+    }
+  };
+
+  // Store the language in local storage and update the direction of the page
+  useEffect(() => {
+    localStorage.setItem("language", language);
+    document
+      .querySelector("html")
+      .setAttribute("dir", language === "ar" ? "rtl" : "ltr");
+  }, [language]);
 
   return (
     <div className="sticky top-0 z-[1] bg-base-200 py-2">
@@ -142,9 +162,9 @@ const Navbar = () => {
           </div>
         )}
 
-        {/*User Section */}
-        <div className="navbar-end hidden lg:flex ">
-          {/* User */}
+        {/* User Section - Visible only on lg screens */}
+        <div className="flex justify-end flex-1 px-2 hidden lg:flex">
+          {/* User Dropdown */}
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <svg
@@ -165,7 +185,7 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              {currentUser ? ( // Conditional rendering based on currentUser
+              {currentUser ? (
                 <>
                   <li className="menu-title">
                     <span className="text-yellow-500">
@@ -194,7 +214,7 @@ const Navbar = () => {
         </div>
 
         {/* Light/Dark Mode Section */}
-        <div className="flex justify-end flex-1 px-2">
+        <div className="lg:px-2 flex justify-end flex-1 px-2 lg:flex-none lg:justify-start">
           <label tabIndex={0} className="btn btn-ghost btn-circle">
             <label className="swap swap-rotate">
               <input
@@ -225,6 +245,72 @@ const Navbar = () => {
               </svg>
             </label>
           </label>
+        </div>
+
+        {/* Language Toggle Dropdown */}
+        <div title="Change Language" className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost flex items-center gap-1"
+            aria-label="Language"
+            style={{
+              whiteSpace: "nowrap",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11 5a.75.75 0 0 1 .688.452l3.25 7.5a.75.75 0 1 1-1.376.596L12.89 12H9.109l-.67 1.548a.75.75 0 1 1-1.377-.596l3.25-7.5A.75.75 0 0 1 11 5Zm-1.24 5.5h2.48L11 7.636 9.76 10.5ZM5 1a.75.75 0 0 1 .75.75v1.261a25.27 25.27 0 0 1 2.598.211.75.75 0 1 1-.2 1.487c-.22-.03-.44-.056-.662-.08A12.939 12.939 0 0 1 5.92 8.058c.237.304.488.595.752.873a.75.75 0 0 1-1.086 1.035A13.075 13.075 0 0 1 5 9.307a13.068 13.068 0 0 1-2.841 2.546.75.75 0 0 1-.827-1.252A11.566 11.566 0 0 0 4.08 8.057a12.991 12.991 0 0 1-.554-.938.75.75 0 1 1 1.323-.707c.049.09.099.181.15.271.388-.68.708-1.405.952-2.164a23.941 23.941 0 0 0-4.1.19.75.75 0 0 1-.2-1.487c.853-.114 1.72-.185 2.598-.211V1.75A.75.75 0 0 1 5 1Z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <svg
+              width="12px"
+              height="12px"
+              className="fill-current opacity-60"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 2048 2048"
+            >
+              <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+            </svg>
+          </div>
+          <div
+            tabIndex={0}
+            className="dropdown-content bg-base-200 text-base-content rounded-box top-px mt-16 max-h-[calc(100vh-10rem)] w-56 overflow-y-auto border border-white/5 shadow-2xl outline outline-1 outline-black/5"
+          >
+            <ul className="menu menu-sm gap-1">
+              <li>
+                <button
+                  className="flex"
+                  onClick={() => handleLanguageToggle("ar")}
+                >
+                  <span className="badge badge-sm badge-outline !pl-1.5 !pr-1 pt-px font-mono !text-[.6rem] font-bold tracking-widest opacity-50">
+                    AR
+                  </span>
+                  <span className="font-[sans-serif]">عربي</span>
+                </button>
+              </li>
+              <li>
+                <button
+                  className="flex"
+                  onClick={() => handleLanguageToggle("en")}
+                >
+                  <span className="badge badge-sm badge-outline !pl-1.5 !pr-1 pt-px font-mono !text-[.6rem] font-bold tracking-widest opacity-50">
+                    EN
+                  </span>
+                  <span className="font-[sans-serif]">English</span>
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
