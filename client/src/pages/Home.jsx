300f9ee2e1adc49e23d6bc76bc5482376c7fdd44
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-09-09 11:53:57 +0300

    Update title color

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,44 +1,59 @@
 // src/components/Home.jsx
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import { useAuth } from '../hooks/useAuth';
-import hero_img_ar from '../assets/logo.png';
-import hero_img_en from '../assets/logo-en.png';
-import Layout from '../components/Layout/Layout';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useAuth } from "../hooks/useAuth";
+import hero_img_ar from "../assets/logo.png";
+import hero_img_en from "../assets/logo-en.png";
+import Layout from "../components/Layout/Layout";
+import { useTranslation } from "react-i18next";
 
 const Home = () => {
   const { currentUser } = useAuth();
   const { t, i18n } = useTranslation(); // Correct use of useTranslation hook
 
   // Determine the text alignment and image based on the current language
-  const isArabic = i18n.language === 'ar';
+  const isArabic = i18n.language === "ar";
   const heroImg = isArabic ? hero_img_ar : hero_img_en; // Choose the image based on the language
+
+  console.log(t("home.welcome").split("Thammenha")[1]);
 
   return (
     <Layout>
       <div className="hero py-12 sm:py-16 bg-base-300">
         <div className="hero-content grid md:grid-cols-2 gap-8">
-          
           {/* Text Block with Creative Animation */}
           <motion.div
             initial={{ opacity: 0, x: -50, rotate: -10 }} // Start off with slight rotation and position
             animate={{ opacity: 1, x: 0, rotate: 0 }} // Rotate to 0 degrees while fading in
-            transition={{ 
-              type: 'spring', 
-              stiffness: 100, 
-              damping: 10, 
-              mass: 0.5, 
-              duration: 0.8 
+            transition={{
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+              mass: 0.5,
+              duration: 0.8,
             }} // Add a bouncy effect
-            className={`md:order-2 ${isArabic ? 'text-right' : 'text-left'}`} // Adjust text alignment
+            className={`md:order-2 ${isArabic ? "text-right" : "text-left"}`} // Adjust text alignment
           >
             <h1
               className="text-4xl sm:text-5xl font-bold leading-normal"
               style={{ fontFamily: "'El Messiri', sans-serif" }}
             >
-              {t('home.welcome')}
+              {/* For English welcome message */}
+              {t("home.welcome").includes("Thammenha") ? (
+                <>
+                  {t("home.welcome").split("Thammenha")[0]}
+                  <span className="text-red-500">Thammenha</span>
+                  {t("home.welcome").split("Thammenha")[1]}
+                </>
+              ) : (
+                // For Arabic welcome message
+                <>
+                  {t("home.welcome").split("ثمنها")[0]}
+                  <span className="text-red-500">ثمنها</span>
+                  {t("home.welcome").split("ثمنها")[1]}
+                </>
+              )}
             </h1>
 
             {/* Image for Small Screens */}
@@ -50,14 +65,14 @@ const Home = () => {
               className="pt-6 text-3xl leading-normal"
               style={{ fontFamily: "'El Messiri', sans-serif" }}
             >
-              {t('home.description')}
+              {t("home.description")}
             </p>
 
             <p
               className="pb-12 text-md leading-normal"
               style={{ fontFamily: "'El Messiri', sans-serif" }}
             >
-              {t('home.coming_soon')}
+              {t("home.coming_soon")}
             </p>
 
             <div className="flex justify-center gap-2">
@@ -66,7 +81,7 @@ const Home = () => {
                 className="btn btn-neutral flex-grow text-xl"
                 style={{ fontFamily: "'El Messiri', sans-serif" }}
               >
-                {t('home.about_us')}
+                {t("home.about_us")}
               </Link>
 
               {!currentUser && (
@@ -75,7 +90,7 @@ const Home = () => {
                   className="btn btn-neutral flex-grow text-xl ml-2"
                   style={{ fontFamily: "'El Messiri', sans-serif" }}
                 >
-                  {t('home.get_started')}
+                  {t("home.get_started")}
                 </Link>
               )}
             </div>
@@ -85,18 +100,17 @@ const Home = () => {
           <motion.div
             initial={{ opacity: 0, x: 50, scale: 0.8, rotate: 10 }} // Start smaller and slightly rotated
             animate={{ opacity: 1, x: 0, scale: 1, rotate: 0 }} // Grow to normal size with a slight bounce
-            transition={{ 
-              type: 'spring', 
-              stiffness: 120, 
-              damping: 12, 
-              mass: 0.5, 
-              duration: 0.8 
+            transition={{
+              type: "spring",
+              stiffness: 120,
+              damping: 12,
+              mass: 0.5,
+              duration: 0.8,
             }} // Add a bouncy and springy effect
             className="order-1 hidden md:block ml-auto md:order-1"
           >
             <img src={heroImg} alt="Hero" className="max-w-full h-auto" />
           </motion.div>
-
         </div>
       </div>
     </Layout>
